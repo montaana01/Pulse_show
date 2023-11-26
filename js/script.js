8afc1797a9bf43ca314161b138f1b5aff57cbb76
montaana01 <yakovlevworkby@gmail.com>
2023-11-26 18:35:55 +0300

    Add tabs

--- js/script.js
+++ js/script.js
@@ -51,5 +51,22 @@ $(document).ready(function () {
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
 
+    // Modal
 
+    $('[data-modal=consultation]').on('click', function () {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function () {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+
+    $('.button_catalog').each(function (i) {
+        $(this).on('click', function () {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
+    $('.button_submit').on('click', function () {
+        $('.overlay, #thanks').fadeIn('slow');
+    })
 });
