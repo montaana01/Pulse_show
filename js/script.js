7a268fc578e817bc4fd81a2db80552bd245962fb
Alexey Yakovlev <yakovlevworkby@gmail.com>
2024-02-25 18:24:39 +0300

    trying_fix_mailer

--- js/script.js
+++ js/script.js
@@ -102,7 +102,7 @@ $(document).ready(function () {
 
         $.ajax({
             type: "POST",
-            url: "mailer/smart.php",
+            url: "smart.php",
             data: $(this).serialize()
         }).done(function() {
             $(this).find("input").val("");
